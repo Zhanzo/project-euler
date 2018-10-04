ad7eef76e64c7d17ca3ca4a026d69a051212f4ce
Robin Andersson <robin182andersson@gmail.com>
2018-10-04 12:00:36 +0200

    Solved problem 25

--- Python/problem25.py
+++ Python/problem25.py
@@ -1,7 +1,19 @@
-x = 2
-z = 0
+def main(num_digits):
+    index = fibonacci(num_digits)
+    print(index)
 
-while z < 10:
-    z = (x - 1) + (x - 2)
-    x += 1
+def fibonacci(num_digits):
+    x = 0
+    y = 1
+    z = 0
+    index = 1
+    while (len(str(z)) < num_digits):
+        z = x + y
+        temp = y
+        y = x + y
+        x = temp
+        index += 1
     print(z)
+    return index
+
+main(1000)
